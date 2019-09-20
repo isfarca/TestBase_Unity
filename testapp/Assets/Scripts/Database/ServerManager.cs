1e1aa95a4033ac9bb442718821b4f4fcc4a17b59
Fethi Isfarca <fisfarca@googlemail.com>
2019-09-20 14:15:16 +0200

    Realtime database reconstruct
    New scene and Server manager added.

---
+++ testapp/Assets/Scripts/Database/ServerManager.cs
@@ -0,0 +1,45 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public static partial class ServerManager
+{
+    // Database reference.
+    public static DatabaseReference ConnectionReference;
+    
+    // Database settings.
+    public static readonly string DATABASE_URL = "https://testbase-a8cea.firebaseio.com/";
+    
+    /// <summary>
+    /// Initialize database.
+    /// </summary>
+    public static void InitDatabase()
+    {
+        // Check dependencies.
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
+        {
+            // Cache Status.
+            var dependencyStatus = task.Result;
+
+            // Firebase available?
+            if (dependencyStatus == Firebase.DependencyStatus.Available) // Yes.
+            {
+                // Output successful.
+                Debug.Log("Firebase available!");
+                
+                // Set Firebase instance.
+                FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(DATABASE_URL);
+                ConnectionReference = FirebaseDatabase.DefaultInstance.RootReference;
+            }
+            else // No.
+            {
+                // Output error.
+                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}!");
+                
+                // Quit software, because Firebase can't be used.
+                Application.Quit();
+            }
+        });
+    }
+}
---
+++ testapp/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton instance.
+    public static GameManager Instance;
+
+    /// <summary>
+    /// Initialize Game manager instance.
+    /// </summary>
+    private void Awake()
+    {
+        // Have you a instance?
+        if (Instance != null && Instance != this) // Yes.
+        {
+            // Kill the newest instance.
+            Destroy(gameObject);
+        } 
+        else // No.
+        {
+            // Set instance and Call app start.
+            Instance = this;
+            OnAppStart();
+        }
+    }
+
+    /// <summary>
+    /// Load important components, by app start.
+    /// </summary>
+    private void OnAppStart()
+    {
+        ServerManager.InitDatabase();
+    }
+
+    /// <summary>
+    /// Clear datas, before quite.
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+    }
+}
